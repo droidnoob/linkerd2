c9f6d91c9c9ffa02a817e44cd822d423ad071d51
Ananthakrishnan <kannan4mi3@gmail.com>
2019-11-10 16:57:10 +0530

    Added error message for tap api check
    Signed-off-by: Ananthakrishnan <kannan4mi3@gmail.com>

--- pkg/healthcheck/healthcheck.go
+++ pkg/healthcheck/healthcheck.go
@@ -1613,7 +1613,7 @@ func (hc *HealthChecker) checkAPIService(serviceName string) error {
 			available = true
 			break
 		}
-		errorMessage = condition.Message
+		errorMessage = fmt.Sprintf("%s:%s", condition.Reason, condition.Message)
 	}
 	if !available {
 		return fmt.Errorf(errorMessage)
