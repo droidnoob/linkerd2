586a9f7357ef7c8d369f1433689ab72769c0a88a
Ananthakrishnan <kannan4mi3@gmail.com>
2020-01-09 13:51:55 +0530

    healthcheck: rebased with master
    Signed-off-by: Ananthakrishnan <kannan4mi3@gmail.com>

--- pkg/healthcheck/healthcheck.go
+++ pkg/healthcheck/healthcheck.go
@@ -847,22 +847,6 @@ func (hc *HealthChecker) allCategories() []category {
 						return hc.checkAPIService(linkerdTapAPIServiceName)
 					},
 				},
-				{
-					description: "tap api service is running",
-					hintAnchor:  "#",
-					warning:     true,
-					check: func(ctx context.Context) error {
-						return hc.checkAPIService(linkerdTapAPIServiceName)
-					},
-				},
-				{
-					description: "tap api service is running",
-					hintAnchor:  "l5d-tap-api",
-					warning:     true,
-					check: func(ctx context.Context) error {
-						return hc.checkAPIService(linkerdTapAPIServiceName)
-					},
-				},
 			},
 		},
 		{
